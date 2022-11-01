eddfea75ac6c7df5567dbc612bea4454a5bc2377
ViacheslavPesterev <pesterevvv@gmail.com>
2022-11-01 22:58:12 +0300

    Решение задач уровня Middle. Категория : 3.3. Hibernate Тема : 3.3.0. Проект "АвтоМаг" 2. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/update_001.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS auto_user (
+     id         SERIAL PRIMARY KEY,
+     login      TEXT NOT NULL,
+     password   TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS auto_post (
+      id          SERIAL PRIMARY KEY,
+      description TEXT NOT NULL,
+      created     TIMESTAMP NOT NULL default now(),
+      auto_user_id INT NOT NULL REFERENCES auto_user(id) ON DELETE CASCADE
+    );
